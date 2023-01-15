15c787fe125bfd21235626919163347f744baa04
Lukasz <lis.lukasz92@gmail.com>
2023-01-15 12:25:47 +0100

    New program
    Works well, one line optimized. Basic program syntax commented.

---
+++ Descending Order.py
@@ -0,0 +1,17 @@
+def descending_order(num: int) -> int:
+    # number_list = []
+    # new_number: str = ""
+    # new_int: int = 0
+    # for n in str(num):
+    #     number_list.append(n)
+    # number_list.sort(reverse=True)
+    new_int = int(''.join(x for x in sorted([n for n in str(num)], reverse=True)))
+    # number_list.sort(reverse=True)
+    # for x in number_list:
+    #     new_number += x
+    print(new_int)
+    return (int(new_int))
+
+
+test_number: int = 12345
+print(descending_order(test_number))
