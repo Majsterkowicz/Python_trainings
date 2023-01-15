d6b4ff64e094e8ddc705a33509c84353553c8460
Lukasz <lis.lukasz92@gmail.com>
2023-01-15 11:07:58 +0100

    Main program modified
    Beta version commented. Main program switched to looping through given string, then to list and type the biggest and lowest number. Then return.

--- Highest_and_Lowest_test.py
+++ Highest_and_Lowest_test.py
@@ -1,11 +1,12 @@
 def highest_and_lowest(numbers: str) -> str:
     number_current: str = ""
-    string_max: str = "0"
-    string_min: str = "0"
-    number_max: int = 0
-    number_min: int = 0
+    # string_max: str = "0"
+    # string_min: str = "0"
+    # number_max: int = 0
+    # number_min: int = 0
     counter: int = 0
-    first_number: bool = True
+    # first_number: bool = True
+    number_list = []
     print(numbers)
 
     for x in numbers:
@@ -15,28 +16,33 @@ def highest_and_lowest(numbers: str) -> str:
             number_current += x
             print("number_current= " + number_current)
         if x == " " or len(numbers) == counter:
-            print("counter" + str(counter))
-            print("spacja")
-            if first_number is True:
-                number_max = int(number_current)
-                number_min = int(number_current)
-                number_current = ""
-                first_number = False
-            elif int(number_current) > number_max:
-                print("number_max" + str(number_max))
-                number_max = int(number_current)
-                print("number_max= " + str(number_max))
-                number_current = ""
-            elif int(number_current) < number_max:
-                if int(number_current) < number_min:
-                    number_min = int(number_current)
-                print("number_min" + str(number_min))
-                print("number_min= " + str(number_min))
-                number_current = ""
-    string_max = str(number_max)
-    string_min = str(number_min)
-    return (string_max + " " + string_min)
+            number_list.append(int(number_current))
+            number_current = ""
+    #         print("counter" + str(counter))
+    #         print("spacja")
+
+            # if first_number is True:
+            #     number_max = int(number_current)
+            #     number_min = int(number_current)
+            #     number_current = ""
+            #     first_number = False
+    #         elif int(number_current) > number_max:
+    #             print("number_max" + str(number_max))
+    #             number_max = int(number_current)
+    #             print("number_max= " + str(number_max))
+    #             number_current = ""
+    #         elif int(number_current) < number_max:
+    #             if int(number_current) < number_min:
+    #                 number_min = int(number_current)
+    #             print("number_min" + str(number_min))
+    #             print("number_min= " + str(number_min))
+    #             number_current = ""
+    # string_max = str(number_max)
+    # string_min = str(number_min)
+    print(number_list)
+    return (str(max(number_list)) + " " + str(min(number_list)))
+    # return (string_max + " " + string_min)
 
 
-test_numbers: str = "-10 -20"
+test_numbers: str = "1 1 10"
 print(highest_and_lowest(test_numbers))
