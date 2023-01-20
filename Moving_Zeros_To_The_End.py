7ad3e4db9faa1ca889006e895f17d73febdba225
Lukasz <lis.lukasz92@gmail.com>
2023-01-20 23:44:55 +0100

    New program
    New program created, works well

---
+++ Moving_Zeros_To_The_End.py
@@ -0,0 +1,20 @@
+# Write an algorithm that takes an array and moves all of the zeros
+# to the end, preserving the order of the other elements.
+
+# move_zeros([1, 0, 1, 2, 0, 1, 3])
+# returns [1, 1, 2, 1, 3, 0, 0]
+
+
+def move_zeros(lst):
+    # if not lst.count(0):
+    #     return lst
+    # else:
+    n: int = lst.count(0)
+    nlst = [i for i in lst if i != 0]
+    for i in range(n):
+        nlst.append(0)
+    return nlst
+
+
+test_list = [1, 0, 1, 2, 0, 1, 3, 5, 0, 5, 0, 0, 0, 5, 2, 3, 65, 2, 6]
+print(move_zeros(test_list))
