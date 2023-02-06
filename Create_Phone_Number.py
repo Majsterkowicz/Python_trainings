a2e13c720f21d950b71def5b5463d572995753fc
Lukasz <lis.lukasz92@gmail.com>
2023-02-06 21:03:59 +0100

    New program
    Some tests with string and int lists. Open to code and solve the kata :)

---
+++ Create_Phone_Number.py
@@ -0,0 +1,23 @@
+# Write a function that accepts an array of 10 integers (between 0 and 9),
+# that returns a string of those numbers in the form of a phone number.
+
+# Example
+# create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) # => returns
+# "(123) 456-7890"
+# The returned format must be correct in order to complete this challenge.
+
+# Don't forget the space after the closing parentheses!
+
+
+def create_phone_number(n: int) -> str:
+    # n = str(n)
+    # txt: str = "({})"
+    # return "".join(txt.format(n[0:3]))
+    # number: str = ""
+    # for x in n:
+    #     number = "".join(str(x))
+    return "".join(str(x) for x in n)
+
+
+test_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+print(create_phone_number(test_array))
