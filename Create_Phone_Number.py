21dc0e9aecc361f0b54d288845e1354368014a0b
Lukasz <lis.lukasz92@gmail.com>
2023-02-06 22:00:35 +0100

    Update
    Some more tests. The solution finally found. To improve probably.

--- Create_Phone_Number.py
+++ Create_Phone_Number.py
@@ -14,9 +14,9 @@ def create_phone_number(n: int) -> str:
     # txt: str = "({})"
     # return "".join(txt.format(n[0:3]))
     # number: str = ""
-    # for x in n:
-    #     number = "".join(str(x))
-    return "".join(str(x) for x in n)
+    # for x in n[:3]:
+    #     number += (str(x))
+    return "(%s) %s-%s" % ("".join(str(x) for x in n[:3]), "".join(str(x) for x in n[3:6]), "".join(str(x) for x in n[6:]))
 
 
 test_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
