058923fecfb9ba1ac03da9aaf6bb6e9bfe57481d
Lukasz <lis.lukasz92@gmail.com>
2023-02-11 09:56:34 +0100

    Program update
    Program works well.

--- Human_readable_format.py
+++ Human_readable_format.py
@@ -1,4 +1,4 @@
-# Write a function, which takes a non-negative integer (seconds)
+# Write a function, which takes a non-negative integer (sec)
 # as input and returns the time in a human-readable format (HH:MM:SS)
 
 # HH = hours, padded to 2 digits, range: 00 - 99
@@ -8,6 +8,13 @@
 
 # You can find some examples in the test fixtures.
 
-def make_readable(seconds):
-    time: str = ""
-    
+def make_readable(sec):
+    h: int = sec // 3600
+    m: int = (sec % 3600) // 60
+    s: int = sec % 60
+    return '%s:%s:%s' % (str(h).zfill(2), str(m).zfill(2), str(s).zfill(2))
+    # return '({}) {}-{}'.format(str1, str2, str3)
+
+
+test_seconds: int = 8666
+print(make_readable(test_seconds))
